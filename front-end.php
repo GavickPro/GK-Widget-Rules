0aed4bb09b161815a402daf2b93ffb437144d289
Tomasz Dziuda <dziudek@gmail.com>
2014-08-19 14:25:43 +0200

    Recoded version of the plugin

---
+++ front-end.php
@@ -0,0 +1,186 @@
+<?php
+
+if(!is_admin() && !class_exists('GK_Widget_Rules_Front_End')) {
+	class GK_Widget_Rules_Front_End {
+		
+		static $conditions = array();
+		/**
+		 *
+		 * Function used to create conditional string
+		 *
+		 * @param mode - mode of the condition - exclude, all, include
+		 * @param input - input data separated by commas, look into example inside the function
+		 * @param users - the value of the user access
+		 *
+		 * @return HTML output
+		 *
+		 **/
+		static function condition($mode, $input, $users) {
+			// Example input:
+			// homepage,page:12,post:10,category:test,tag:test
+		
+			$output = ' (';
+			if($mode == 'all') {
+				$output = '';
+			} else if($mode == 'exclude') {
+				$output = ' !(';
+			}
+		
+			if($mode != 'all') {
+				$input = preg_replace('@[^a-zA-Z0-9\-_,;\:\.\s]@mis', '', $input); 
+				$input = substr($input, 1);
+				$input = explode(',', $input);
+		
+				for($i = 0; $i < count($input); $i++) {
+					if($i > 0) {
+						$output .= '||'; 
+					}
+		
+					if(stripos($input[$i], 'homepage') !== FALSE) {
+					    $output .= ' is_home() ';
+					} else if(stripos($input[$i], 'page:') !== FALSE) {
+					    $output .= ' is_page(\'' . substr($input[$i], 5) . '\') ';
+					} else if(stripos($input[$i], 'post:') !== FALSE) {
+					    $output .= ' is_single(\'' . substr($input[$i], 5) . '\') ';
+					} else if(stripos($input[$i], 'category:') !== FALSE) {
+					    $output .= ' (is_category(\'' . substr($input[$i], 9) . '\') || (in_category(\'' . substr($input[$i], 9) . '\') && is_single())) ';
+					} else if(stripos($input[$i], 'category_descendant:') !== FALSE) {
+						$output .= ' (is_category(\'' . substr($input[$i], 20) . '\') || (in_category(\'' . substr($input[$i], 20) . '\') || post_is_in_descendant_category( \'' . substr($input[$i], 20) . '\' ) && !is_home())) ';
+					} else if(stripos($input[$i], 'tag:') !== FALSE) {
+					    $output .= ' (is_tag(\'' . substr($input[$i], 4) . '\') || (has_tag(\'' . substr($input[$i], 4) . '\') && is_single())) ';
+					} else if(stripos($input[$i], 'archive') !== FALSE) {
+					    $output .= ' is_archive() ';
+					} else if(stripos($input[$i], 'author:') !== FALSE) {
+					    $output .= ' (is_author(\'' . substr($input[$i], 7) . '\')) ';
+				    } else if(stripos($input[$i], 'template:') !== FALSE) {
+				        if(substr($input[$i], 9) != '') {
+				       		$output .= ' (is_page_template(\'' . substr($input[$i], 9) . '.php\') && is_singular()) ';
+				       	} else {
+				       		$output .= ' (is_page_template() && is_singular()) ';
+				       	}
+			        } else if(stripos($input[$i], 'format:') !== FALSE) {
+			        	if(substr($input[$i], 7 != '')) {
+			            	$output .= ' (has_term( \'post_format\', \'post-format-' . substr($input[$i], 7) . '\') && is_single()) ';
+			            } else {
+			            	$output .= ' (has_term( \'post_format\') && is_single()) ';
+			            }
+					} else if(stripos($input[$i], 'taxonomy:') !== FALSE) {
+					    if(substr($input[$i], 9) != '') {
+					    	$taxonomy = substr($input[$i], 9);
+					    	$taxonomy = explode(';', $taxonomy);
+					    	// check amount of taxonomies
+					    	if(count($taxonomy) == 1) {
+					    	     $output .= ' (is_tax(\'' . $taxonomy[0] . '\'))';
+					    	} else if(count($taxonomy) == 2) {
+					    	     $output .= ' (has_term(\'' . $taxonomy[1] . '\', \'' . $taxonomy[0] . '\')) ';
+					    	}
+					   	}
+					} else if(stripos($input[$i], 'posttype:') !== FALSE) {
+					    if(substr($input[$i], 9) != '') {
+					    	$type = substr($input[$i], 9);
+					    	// check for post types
+					    	if($type != '') {
+					   			$output .= ' (get_post_type() == \'' . $type . '\' && is_single()) ';
+					   		}
+					   	}
+					} else if(stripos($input[$i], 'search') !== FALSE) {
+					    $output .= ' is_search() ';
+					} else if(stripos($input[$i], 'page404') !== FALSE) {
+					    $output .= ' is_404() ';
+					}
+				}
+		
+				$output .= ')';
+			}
+		
+			if($users != 'all') {
+				if($users == 'guests') {
+					$output .= (($output == '') ? '' : ' && ') . ' !is_user_logged_in()';
+				} else if($users == 'registered') {
+					$output .= (($output == '') ? '' : ' && ') . ' is_user_logged_in()';
+				} else if($users == 'administrator') {
+					$output .= (($output == '') ? '' : ' && ') . ' current_user_can(\'manage_options\')';
+				}
+			}
+		
+			if($output == '' || trim($output) == '()' || trim($output) == '!()') {
+				$output = ' TRUE';
+			}
+			
+			return $output;
+		}
+		
+		static function filter_widgets($sidebars_widgets) {	
+			// iterate all sidebars
+			foreach($sidebars_widgets as $sidebar => $widgets) {	
+				// skip inactive and empty sidebars
+				if ($sidebar == 'wp_inactive_widgets' || empty($widgets)) {
+					continue;
+				}
+				// get all widgets
+				foreach($widgets as $index => $id) {	
+					// get settings
+					$config = $sidebar_widgets[$sidebar][$index]['gk_widget_rules'];
+					// create function
+					$type = '';
+					if(isset($config['type'])) {
+						$type = $config['type'];
+					}
+					
+					$rules = '';
+					if(isset($config['rules'])) {
+						$rules = $config['rules'];
+					}
+					
+					$users = '';
+					if(isset($config['users'])) {
+						$users = $config['users'];
+					}
+					// cache for conditions
+					if(!isset(self::$conditions[$id])) {
+						self::$conditions[$id] = self::condition($type, $rules, $users);
+					}
+					
+					$conditional_function = create_function('', 'return '. self::$conditions[$id] .';');
+					
+					// generate the result of function
+					$conditional_result = $conditional_function();
+					// eval condition function
+					if(!$conditional_result) {
+						unset($sidebars_widgets[$sidebar][$index]);
+						continue;
+					}
+				}
+			}
+			
+			return $sidebars_widgets;
+		}
+		
+		// function used to add new CSS classes to widgets
+		static function add_classes($params) {
+			// additional CSS classes
+			/*if(
+				isset($config['css']) && 
+				isset($config['css'][$params[0]['widget_id']])
+			) {
+				$widget_css_class = $config['css'][$params[0]['widget_id']];
+				$params[0]['before_widget'] = str_replace('class="', 'class="' . $widget_css_class . ' ', $params[0]['before_widget']);
+			}
+			// responsive CSS classes
+			if(
+				isset($config['responsive']) && 
+				isset($config['responsive'][$params[0]['widget_id']])
+			) {
+				$widget_rwd_css_class = $config['responsive'][$params[0]['widget_id']];
+				$params[0]['before_widget'] = str_replace('class="', 'class="' . $widget_rwd_css_class . ' ', $params[0]['before_widget']);
+			}*/
+			
+			return $params;
+		}
+	}
+	
+	add_filter('sidebars_widgets', array('GK_Widget_Rules_Front_End', 'filter_widgets'), 10);
+	add_filter('dynamic_sidebar_params', array('GK_Widget_Rules_Front_End', 'add_classes'), 10);
+}
+
+// EOF
--- gk-widget-rules.js
+++ gk-widget-rules.js
@@ -5,129 +5,35 @@
  * -------------------------------------------
  *
  **/
+ 
 "use strict";
-
-/**
- * jQuery Cookie plugin
- *
- * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
- * Dual licensed under the MIT and GPL licenses:
- * http://www.opensource.org/licenses/mit-license.php
- * http://www.gnu.org/licenses/gpl.html
- *
- */
-jQuery.cookie = function (key, value, options) {
-    // key and at least value given, set cookie...        
-    if (arguments.length > 1 && String(value) !== "[object Object]") {        
-        options = jQuery.extend({}, options);
-                
-        if (value === null || value === undefined) {            
-            options.expires = -1;        
-        }
-                
-        if (typeof options.expires === 'number') {            
-            var days = options.expires,
-                t = options.expires = new Date();            
-            t.setDate(t.getDate() + days);        
-        }
-                
-        value = String(value);
-
-        return (document.cookie = [encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value), options.expires ? '; expires=' + options.expires.toUTCString() : '', options.path ? '; path=' + options.path : '', options.domain ? '; domain=' + options.domain : '', options.secure ? '; secure' : ''        
-        ].join(''));    
-    }
-    // key and possibly options given, get cookie...   
-    options = value || {};    
-    var result, decode = options.raw ? function (s) {
-            return s;
-        } : decodeURIComponent;    
-    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
-};
-
-// remove active state from any widget rules wrapper on the start to avoid
-// the situation when some wrapper is opened earlier due saved data in the cookie file.
-jQuery(window).on('beforeunload', function () {
-    jQuery.cookie('gk_last_opened_widget_rules_wrap', 0, {
-        expires: 365,
-        path: '/'
-    });
-});
 
 // added event to open the widget rules wrapper (uses id from the back-end wihout the random ID at end).
 jQuery(document).click(function (e) {
-    if (jQuery(e.target).hasClass('gk_widget_rules_btn')) {
+    if (jQuery(e.target).hasClass('gk_widget_rules_btn') && jQuery(e.target).hasClass('button')) {
         var wrap = jQuery(e.target).next('.gk_widget_rules_wrapper');
 
         if (wrap.hasClass('active')) {
             wrap.removeClass('active');
-            jQuery.cookie('gk_last_opened_widget_rules_wrap', 0, {
-                expires: 365,
-                path: '/'
-            });
         } else {
             wrap.addClass('active');
-            jQuery.cookie('gk_last_opened_widget_rules_wrap', wrap.attr('data-id'), {
-                expires: 365,
-                path: '/'
-            });
         }
     }
 });
 
-function gk_widget_control_init(id, inner) {
-    // check if the widget isn't a new widget
-    var allForms = jQuery('.gk_widget_rules_form');
-    var newest = null;
-    var flag = 0;
-
-    if (!inner) {
-        for (var i = 0; i < allForms.length; i++) {
-            if ('#' + jQuery(allForms[i]).attr('id') === id) {
-                newest = jQuery(allForms[i]);
-                flag += 1;
-            }
-        }
-
-        if (flag > 1) {
-            newest.attr('id', newest.attr('id') + '-' + Math.floor((Math.random() * 10000 + 1)));
-            newest.attr('data-state', 'uninitialized');
-            gk_widget_control_init('#' + newest.attr('id'), true);
-            return;
-        }
-    }
-    // if it is a new widget
-    if (inner) {
-        var mouseUpEvent = function () {
-            setTimeout(function () {
-                gk_widget_control_init_events(id, inner);
-                jQuery(document).unbind('mouseup', mouseUpEvent);
-            }, 250);
-        };
-
-        jQuery(document).bind('mouseup', mouseUpEvent);
-    } else {
-        gk_widget_control_init_events(id, inner);
-    }
+function gk_widget_control_init(id) {
+    gk_widget_control_init_events(id);
 }
 // function to init form event
 
-function gk_widget_control_init_events(id, inner) {
+function gk_widget_control_init_events(id) {
     var form = jQuery(id);
-
-    if (inner) {
-        form.parent().find('select:last-child').css('opacity', '0.5');
-
-        setTimeout(function () {
-            var btn = form.parent().parent().parent().find('*[name="savewidget"]');
-            btn.click();
-        }, 1000);
-    }
 
     if (form.attr('data-state') !== 'initialized') {
         form.attr('data-state', 'initialized');
-        var firstSelect = form.parent().find('.gk_widget_rules_select');
-        var select = form.children('.gk_widget_rules_form_select');
-        var btn = form.find('.gk_widget_rules_btn');
+        var firstSelect = form.find('select[name="gk_widget_rules_type"]');
+        var select = form.find('.gk_widget_rules_form_select');
+        var btn = form.find('.gk_widget_rules_form .gk_widget_rules_btn');
         var form_inputs = {};
         var form_inputs_names = ['page', 'post', 'category', 'category_descendant', 'tag', 'template', 'taxonomy', 'taxonomy_term', 'posttype', 'author'];
         jQuery.each(form_inputs_names, function (i, el) {
@@ -135,16 +41,17 @@ function gk_widget_control_init_events(id, inner) {
         });
         // hide unnecesary form
         if (firstSelect.children('option:selected').val() === 'all') {
-            form.css('display', 'none');
+            form.find('.gk_widget_rules_form').css('display', 'none');
         }
         // change event
         firstSelect.change(function () {
             var value = firstSelect.children('option:selected').val();
+            var fieldset = form.find('.gk_widget_rules_form');
 
             if (value === 'all') {
-                form.css('display', 'none');
+                fieldset.css('display', 'none');
             } else {
-                form.css('display', 'block');
+                fieldset.css('display', 'block');
             }
         });
         // refresh the list
@@ -257,30 +164,6 @@ function gk_widget_control_init_events(id, inner) {
                 gk_widget_control_refresh(form);
             }
         });
-        // event to display the custom CSS class field 
-        var selectStyles = jQuery(document).find('.gk_widget_rules_select_styles');
-        selectStyles.each(function (i, select) {
-            select = jQuery(select);
-
-            if (!select.hasClass('initialized')) {
-                select.change(function () {
-                    var value = select.children('option:selected').val();
-                    var field = select.parent().parent().next('p');
-
-                    if (value !== 'gkcustom') {
-                        if (!field.hasClass('gk-unvisible')) {
-                            field.addClass('gk-unvisible');
-                        }
-                    } else {
-                        if (field.hasClass('gk-unvisible')) {
-                            field.removeClass('gk-unvisible');
-                        }
-                    }
-                });
-
-                select.addClass('initialized');
-            }
-        });
     }
 }
 
@@ -333,3 +216,5 @@ function gk_widget_control_refresh(form) {
         form.find('.gk_widget_rules_nopages').css('display', pages_exist ? 'none' : 'block');
     }
 }
+
+// EOF
