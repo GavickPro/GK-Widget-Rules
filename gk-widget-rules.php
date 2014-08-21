5428462663e5e9bbdf48e92293fbd0abe0419af1
Tomasz Dziuda <dziudek@gmail.com>
2014-08-21 14:15:20 +0200

    Improved localization support.

--- gk-widget-rules.php
+++ gk-widget-rules.php
@@ -14,8 +14,16 @@ Domain Path:   /languages/
 
 global $pagenow;
 
-load_plugin_textdomain( 'gk-widget-rules', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+/**
+ * i18n - language files should be like gk-widget-rules-en_GB.po and gk-widget-rules-en_GB.mo
+ */
+add_action( 'plugins_loaded', 'gk_widget_rules_load_textdomain' );
 
+function gk_widget_rules_load_textdomain() {
+    load_plugin_textdomain( 'gk-widget-rules', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' ); 
+}
+
+// Loading necessary classes
 include dirname( __FILE__ ) . "/back-end.php";
 include dirname( __FILE__ ) . "/front-end.php";
 
