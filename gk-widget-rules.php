6a1ba9d9bee1b2abda8a9dbadc1f69557c6ac0e0
Tomasz Dziuda <dziudek@gmail.com>
2014-08-25 09:51:31 +0200

    Update gk-widget-rules.php

--- gk-widget-rules.php
+++ gk-widget-rules.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:    GK Widget Rules
 Plugin URI:     http://wordpress.org/extend/plugins/gk-widget-rules/
-Description:    Control widgets with WP's conditional tags is_home etc
+Description:    Adds possibility to manage the widgets visibility, their CSS classes etc.
 Version:        1.0.0
 Author:         GavickPro
 Author URI:     http://www.gavick.com
